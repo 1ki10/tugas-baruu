f58b7e461e0b4d0b8439cda458ddee7b0a82bbae
1ki10 <rifqiokta10@gmail.com>
2024-05-22 06:38:09 +0700

    1

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	//fmt.Println("Hello World")
+
+	/* Input (angka) -> menerima input / masukan
+	dari luar ke dalam program */
+
+	// Menerima input ANGKA dari user
+	var angka int
+	fmt.Scanln(&angka)
+
+	fmt.Println("Hasil inputya adalah", angka)
+
 }
