a755ad738cb436a74b20b6f8fecf8046eb594ebb
1ki10 <rifqiokta10@gmail.com>
2024-05-22 08:13:59 +0700

    1

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello wold")
+	var nama string
+	fmt.Scanln(%nama)
+
+	fmt.Println("hello nama saya", nama, "senang berkenalan dengan kalian")
 }
