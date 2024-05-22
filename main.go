35f71450a7c7b4a1c1973ca5b893d0e5ee9a25e0
1ki10 <rifqiokta10@gmail.com>
2024-05-22 07:03:24 +0700

    1

--- main.go
+++ main.go
@@ -3,20 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+
+	//fmt.Println("Hello World")
 
 	/* Input (angka) -> menerima input / masukan
 	dari luar ke dalam program */
 
 	// Menerima input ANGKA dari user
+	//var angka int
+	//fmt.Scanln(&angka)
+
+	//fmt.Println("Hasil inputya adalah", angka)
+
+	//var nama string
+	//fmt.Scanln(&nama)
+
+	//fmt.Println("Hasil inputnya adalah", nama)
+
+	//var kondisi bool
+	//fmt.Println("Isi variable kondisi", kondisi)
+
 	var angka int
+	fmt.Print("masukkan angka untuk dicek: ")
 	fmt.Scanln(&angka)
 
-	fmt.Println("Hasil inputya adalah", angka)
-
-	var nama string
-	fmt.Scanln(&nama)
-
-	fmt.Println("Hasil inputnya adalah", nama)
+	if angka%2 == 0 {
+		fmt.Println("Genap")
+	} else {
+		fmt.Println("ganjil")
+	}
 
 }
